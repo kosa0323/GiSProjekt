0538759136d4034c5d142fcb95c4a08173a6d175
Mateusz Kossakowski <mateusz9423@gmail.com>
2016-05-11 14:33:36 +0200

    MAcierzWag

---
+++ AlgorytmMrówkowy/AlgorytmMrówkowy/AlgorytmMrówkowy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorytmMrówkowy
+{
+    class AlgorytmMrówkowy
+    {
+        public void Wykonaj()
+        {
+            List<double>[,] macierzWag = new List<double>[50, 50];
+            macierzWag[0, 0] = new List<double>();
+            macierzWag[0, 0].Add(12123.543);
+        }
+    }
+}
